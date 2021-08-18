a447f41ed0c94910f42379e8d026f78651b5788c
JCJord <julinnn4k@live.com>
2021-08-17 21:09:20 -0300

    style: edit and delete button add to post list

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatDividerModule } from '@angular/material/divider'
 import { MatExpansionModule } from '@angular/material/expansion'
+
 @NgModule({
   exports: [
     MatFormFieldModule,
