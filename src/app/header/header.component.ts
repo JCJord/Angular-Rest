78f0f853b39cc5a15a1cdf29dfeb777221df8232
JCJord <julinnn4k@live.com>
2021-09-07 13:15:52 -0300

    style: update login form

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core'
 })
 export class HeaderComponent {
   status = false
-
+  constructor (public router: Router) {}
   toggleMenu () {
     this.status = !this.status
   }
