4475965af63f913fcc03badab8fed031bcc52400
JCJord <julinnn4k@live.com>
2021-09-10 15:59:24 -0300

    style: login & signup screens UI update

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-signup',
+
   templateUrl: './signup.component.html',
   styleUrls: ['../login/login.component.css', './signup.component.css']
 })
