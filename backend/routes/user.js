e4a15e9d27f1c98003bd3c956aed43da46711adc
JCJord <julinnn4k@live.com>
2021-09-25 13:31:33 -0300

    style: spinner on log in update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,68 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const UserController = require("../controllers/user")
 
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const user = require("../models/user");
+router.post("/signup",UserController.createUser );
+router.post("/login", UserController.userLogin);
 
-router.post("/signup", (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = new User({
-      email: req.body.email,
-      password: hash,
-    });
-
-    user
-      .save()
-      .then((result) => {
-        res.status(201).json({
-          result: result,
-        });
-      })
-      .catch((err) => {
-        res
-          .status(500)
-          .json({ message: "Invalid Authentication credentials!" });
-      });
-  });
-});
-router.post("/login", (req, res) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({
-          message: "Auth Failed",
-        });
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then((result) => {
-      if (!result) {
-        return res.status(401).json({
-          message: "Invalid authentication credentials",
-        });
-      }
-
-      let token = jwt.sign(
-        { email: fetchedUser.email, userId: fetchedUser._id },
-        "test_page_now",
-        { expiresIn: "1h" }
-      );
-
-      res.status(200).json({
-        token: token,
-        expiresIn: 3600000,
-        userId: fetchedUser._id,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(401).json({
-        message: err,
-      });
-    });
-});
 module.exports = router;
